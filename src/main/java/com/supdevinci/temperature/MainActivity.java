38ea26e7a59003e660cb2c216521cda743a75d58
rdourepoulle <rajekevin@hotmail.fr>
2018-02-27 15:03:21 +0100

    First App

--- src/main/java/com/supdevinci/temperature/MainActivity.java
+++ src/main/java/com/supdevinci/temperature/MainActivity.java
@@ -2,14 +2,19 @@ package com.supdevinci.temperature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     //Déclaration des propriétés
-    EditText  editTextCelcius, editTextFarhremeit;
-    Button    buttonSave;
-    ListView  ListViewTemperature;
+    EditText editTextCelcius, editTextFarhremeit;
+    Button buttonSaveS;
+    ListView ListViewTemperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,31 @@ public class MainActivity extends AppCompatActivity {
 
         editTextCelcius = findViewById(R.id.editTextCelcius);
         editTextFarhremeit = findViewById(R.id.editTextFarehneit);
-        
+        buttonSaveS = findViewById(R.id.buttonSaveS);
+        ListViewTemperature = findViewById(R.id.ListViewTemperature);
 
-        setContentView();
+         editTextCelcius.addTextChangedListener(new TextWatcher() {
+             @Override
+             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+             }
+
+             @Override
+             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+             }
+
+             @Override
+             public void afterTextChanged(Editable editable) {
+
+                 String valeur = editable.ToString();
+                 editTextFarhremeit.setText(valeur);
+
+             }
+         });
+
+
+
+        //setContentView();
     }
 }
